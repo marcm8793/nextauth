e8784b90fb7a14ba85ab812cfba522e566b77581
marcm8793 <marcm9387@gmail.com>
2024-01-04 21:46:23 +0100

    Add dependencies and implement user registration

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,8 @@
 
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +12,26 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUser) {
+    return { error: "Email already exists" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  //TODO: send verification token email
+
+  return { success: "User created" };
 };
