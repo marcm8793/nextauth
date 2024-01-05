dda3e734bbbdb224b7148573aea98b2b0361569f
marcm8793 <marcm9387@gmail.com>
2024-01-05 20:57:33 +0100

    Add next-auth package and import getUserByEmail function
    Update next-auth to version 5.0.0-beta.4

---
+++ routes.ts
@@ -0,0 +1,27 @@
+/**
+ * An array of routes tht are accessible to the public.
+ * These routes do not require authentication.
+ * @type {string[]}
+ */
+export const publicRoutes = ["/"];
+
+/**
+ * An array of routes tht are used for authentication.
+ * These routes will redirect users to /settings
+ *
+ * @type {string[]}
+ */
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+/**
+ * The prefix for API authentication routes.
+ * Routes that start with this prefix are used for API authentication purposes.
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default redirect path after a user logs in.
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
