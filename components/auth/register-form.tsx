b035f32c115a1e1f0dacc8291a02adf84c7f83a0
marcm8793 <marcm9387@gmail.com>
2024-01-05 22:19:21 +0100

    Update authentication and registration logic

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -50,7 +50,7 @@ const RegisterForm = () => {
     <CardWrapper
       headerLabel="Create an account"
       backButtonLabel="Already have an account?"
-      backButtonHref="/auth/register"
+      backButtonHref="/auth/login"
       showSocial
     >
       <Form {...form}>
