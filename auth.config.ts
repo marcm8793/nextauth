46ed26498ed05c603ad0e7907bbe85a8fbe0f8ab
marcm8793 <marcm9387@gmail.com>
2024-01-06 08:07:03 +0100

    Add verification token generation and update success message

--- auth.config.ts
+++ auth.config.ts
@@ -3,9 +3,19 @@ import { LoginSchema } from "./schemas";
 import type { NextAuthConfig } from "next-auth";
 import { getUserByEmail } from "./data/user";
 import bcrypt from "bcryptjs";
+import github from "next-auth/providers/github";
+import google from "next-auth/providers/google";
 
 export default {
   providers: [
+    github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -5,7 +5,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { LoginSchema } from "@/schemas";
 import { useState, useTransition } from "react";
-
 import {
   Form,
   FormControl,
@@ -19,9 +18,15 @@ import { Button } from "../ui/button";
 import { FormError } from "../form-error";
 import { FormSucess } from "../form-success";
 import { login } from "@/actions/login";
-import { start } from "repl";
+import { useSearchParams } from "next/navigation";
 
 const LoginForm = () => {
+  const searchParamas = useSearchParams();
+  const urlError =
+    searchParamas.get("error") === "OAuthAccountNotLinked"
+      ? "Email already in use with different provider"
+      : "";
+
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
   const [isPending, startTransition] = useTransition();
@@ -40,8 +45,9 @@ const LoginForm = () => {
 
     startTransition(() => {
       login(values).then((data) => {
-        setError(data.error);
-        setSuccess(data.success);
+        setError(data?.error);
+        //TODO: Add when we add 2FA
+        //setSuccess(data?.success);
       });
     });
   };
@@ -93,7 +99,7 @@ const LoginForm = () => {
               )}
             />
           </div>
-          <FormError message={error} />
+          <FormError message={error || urlError} />
           <FormSucess message={success} />
 
           <Button type="submit" className="w-full" disabled={isPending}>
